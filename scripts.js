b0457101478b86504738d0de98fc576f844bc820
SONAI <mareg200552@gmail.com>
2023-10-15 19:01:04 +0300

    Added Html and Css, also provided scripts to add, remove tasks. Realised adding tasks with pressing the Enter button

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+inputBox.addEventListener("keyup", function (e) {
+  if (event.code === "Enter") {
+    // event.preventDefault();
+    addTask();
+  }
+});
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("The task is empty");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName == "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
